79071046905a5ad5a673d7b21973a21362d1ee93
Serhii-Onykiienko <onikienkosergey@gmail.com>
2026-08-19 13:16:42 +0300

    add api

--- my-next-app/app/notes/page.tsx
+++ my-next-app/app/notes/page.tsx
@@ -1,7 +1,21 @@
 // app/notes/page.tsx
 
-const Notes = () => {
-  return <div>Notes</div>;
+import NoteList from '@/components/NoteList/NoteList';
+import { getNotes } from '@/src/lib/api';
+
+// 1. Імпортуємо функцію
+
+// 2. Робимо фукнцію асинхронною
+const Notes = async () => {
+  // 3. Виконуємо запит
+  const response = await getNotes();
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+    </section>
+  );
 };
 
 export default Notes;
---
+++ my-next-app/app/page.tsx
@@ -0,0 +1,7 @@
+// app/notes/page.tsx
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
